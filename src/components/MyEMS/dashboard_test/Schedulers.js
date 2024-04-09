b7e556906e3f6a9b2a38b036790730c3e33e3afd
“Lim-son-ui” <jason8511112003@gmail.com>
2024-04-09 17:29:46 +0800

    0409noontonight

--- src/components/MyEMS/dashboard_test/Schedulers.js
+++ src/components/MyEMS/dashboard_test/Schedulers.js
@@ -6,31 +6,42 @@ import { EVENTS } from "./events";
 export default function Schedulers() {
 
   return (
-    <Scheduler events={EVENTS} />
+    //<Scheduler events={EVENTS} />
+    <Scheduler
+      height={500}
+      events={EVENTS}
+      //hourFormat="24" // 將時間格式設置為 24 小時制
+      day={{
+        navigation: true,
+        // hourRenderer:
+        startHour:0,
+        endHour:24,
+        step: 60
+        // cellRenderer:{
+        //   start:0,
+        //   end:3,
+        // }
+      }}
+      week={{
+        startHour:0,
+        endHour:24,
+      }}
+      fields={ 
+        [{
+          name: "description", 
+          type: "input" , 
+          config: { 
+            label: "Description", 
+            required: true, 
+            min: 3, 
+            email: true, 
+            variant: "outlined"
+          }
+        }]
+      }
+      
+      
+    />
   );
-    // <Scheduler events={EVENTS} />
-    // <Scheduler
-    //   height={500}
-    //   events={EVENTS}
-    //   hourFormat="24" // 將時間格式設置為 24 小時制
-    //   // day={{
-    //   //   start: "05:00", // 設置一天的開始時間為 09:00 AM
-    //   //   end: "23:00", // 設置一天的結束時間為 04:00 PM
-    //   // }}
-    //   fields={ 
-    //     [{
-    //       name: "description", 
-    //       type: "input" , 
-    //       config: { 
-    //         label: "Description", 
-    //         required: true, 
-    //         min: 3, 
-    //         email: true, 
-    //         variant: "outlined"
-    //       }
-    //     }]
-    //   }
-    //   // getRemoteEvents={[]}
-    // />
-  
+   
 }
